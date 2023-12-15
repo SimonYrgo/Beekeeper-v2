950edef313476d839226beb3904820158401da52
SimonYrgo <149164321+SimonYrgo@users.noreply.github.com>
2023-12-15 19:12:36 +0100

    backup before hdrp convert

--- Assets/Scripts/DontDestroy.cs
+++ Assets/Scripts/DontDestroy.cs
@@ -7,7 +7,20 @@ public class DontDestroy : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        DontDestroyOnLoad(gameObject); // Gör så detta gameobjektet överlever scenbyte. Ska bara finnas på Canvas Outdoors, level-scenen tror jag? 
+        if (GameManager.Instance.canvas == null)
+        {
+            DontDestroyOnLoad(gameObject); // Gör så detta gameobjektet överlever scenbyte. Ska bara finnas på Canvas Outdoors, level-scenen tror jag? 
+            GameManager.Instance.canvas = gameObject;
+
+        }
+
+        else
+        {
+            Destroy(gameObject);
+        }
+
+
+
     }
 
     // Update is called once per frame
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,12 +6,22 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager Instance; // att skapa en Instance betyder att vi skapar en version av klassen i världen. Alla script kan komma åt den, utan att referera till via slot eller script. Den är "public-public" 
     public float cameraSpeed; // Syftet med detta script är att vi vill spara denna variabels värde, så den inte förstörs vid scenbyte? 
-
+    public GameObject canvas;
     
     private void Awake() // Detta händer innan Start-metoden. Kanske bra försäkran för att DontDestroyOnLoad ska hinna köras? 
     {
-        Instance = this; // när vi skapar en GameManager Instance skapar vi en slot för GameManager Instance. Instance = this fyller den sloten med gameobjektet detta script sitter på? 
+        if (Instance == null)
+        {
+            
+            Instance = this; // när vi skapar en GameManager Instance skapar vi en slot för GameManager Instance. Instance = this fyller den sloten med gameobjektet detta script sitter på? 
+            DontDestroyOnLoad(gameObject); // Förstör inte detta objekt vid scenbyte
 
-        DontDestroyOnLoad(gameObject); // Förstör inte detta objekt vid scenbyte
+        }
+
+        else
+        {
+            Destroy(gameObject);
+        }
+        
     }
 }
